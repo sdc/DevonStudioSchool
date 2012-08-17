39e2a1330faa02e2fc60b85a8e1268a2f48c7b8a
Brian Crocker <briancrocker@southdevon.ac.uk>
2012-08-17 14:18:07 +0100

    added deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+default_run_options[:pty] = true
+
+if ENV['environment'] == "production"
+  set :application, ""
+  role :app, ""
+  role :web, ""
+  role :db,  "", :primary => true
+else
+  set :application, "devonstudioschool.co.uk"
+  role :app, "172.20.1.12"
+  role :web, "172.20.1.12"
+  role :db, "172.20.1.12", :primary => true
+end
+
+set :repository,  "git@github.com:briancrocker81/DevonStudioSchool.git"
+set :branch,      "master"
+set :deploy_to, "/srv/#{application}"
+set :scm, :git
+set :deploy_via, :remote_cache
+
+namespace :deploy do
+  %W(start stop restart migrate finalize_update).each do |event|
+    task event do
+      # don't
+    end
+  end
+end
+
+after "deploy:create_symlink" do
+  run "cp #{shared_path}/configuration.php #{current_path}/"
+  run "cp #{shared_path}/.htaccess #{current_path}/"
+  ["images","attachments"].each do |d|
+    run "rm -rvf #{current_path}/#{d}"
+    run "ln -s #{shared_path}/#{d} #{current_path}/#{d}"
+  end
+  run "chmod a+rw -R #{current_path}"
+end
