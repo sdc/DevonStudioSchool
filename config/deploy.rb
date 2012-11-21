9eddcf23b337f56bd1133597f48c83535e574187
Brian Crocker <briancrocker@southdevon.ac.uk>
2012-11-21 12:51:55 +0000

    whoopy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,20 @@
 default_run_options[:pty] = true
 if ENV['environment'] == "production"
-  set :application, "devonstudioschool.co.uk"
+  set :application, "devonstudioschool"
   role :app, "172.20.1.12"
   role :web, "172.20.1.12"
   role :db, "172.20.1.12", :primary => true
+else
+  set :application, "dss"
+  role :app,  "webdev.southdevon.ac.uk"
+  role :web,  "webdev.southdevon.ac.uk"
+  role :db,   "webdev.southdevon.ac.uk", :primary => true
 end
 
 set :repository,  "git@github.com:briancrocker81/DevonStudioSchool.git"
 set :branch,      "master"
 set :deploy_to, "/srv/#{application}"
 set :scm, :git
-set :deploy_via, :remote_cache
 
 namespace :deploy do
   %W(start stop restart migrate finalize_update).each do |event|
